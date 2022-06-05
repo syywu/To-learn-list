138cd17ca8b1b2749751f559d69cf589107a5800
Samantha Wu <samanthayywu@gmail.com>
2022-06-05 22:14:12 +0100

    notfound

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Create from "./routes/Create";
 import "./App.css";
 import Navbar from "./routes/Navbar";
+import Home from "./routes/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
+        <Home />
         <Create />
       </div>
     </div>
---
+++ src/routes/NotFound.js
@@ -0,0 +1,7 @@
+import App from "../App";
+
+const NotFound = () => {
+  return <h1>NotFound</h1>;
+};
+
+export default NotFound;
