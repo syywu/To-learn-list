b50bdc70e26c8cb68b327881f514828a054db223
Samantha Wu <samanthayywu@gmail.com>
2022-06-05 22:38:30 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import Create from "./routes/Create";
 import "./App.css";
 import Navbar from "./routes/Navbar";
 import Home from "./routes/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from "./routes/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
-        <Create />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </div>
   );
