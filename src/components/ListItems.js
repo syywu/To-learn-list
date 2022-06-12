5f008f906a21829c29bb42569bf7cc0b792f7b39
Samantha Wu <samanthayywu@gmail.com>
2022-06-12 22:28:12 +0100

    all changes

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,9 +1,8 @@
 function Listitems(props) {
   const text = props.text;
-  const title = props.title;
+
   return (
     <div className="list-itmes">
-      <h2>{title}</h2>
       <li>{text}</li>
     </div>
   );
