364f7ea0d505b5b306d4f54db17006320e663609
Samantha Wu <samanthayywu@gmail.com>
2022-06-16 22:05:53 +0100

    submit

--- src/routes/Create.js
+++ src/routes/Create.js
@@ -1,10 +1,22 @@
-const Create = () => {
+import { setState } from "react";
+const Create = ({ handleSubmit }) => {
+  const [subject, setSubject] = setState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="create">
       <h2>Add a new subject</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>New Subject</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={subject}
+          onChange={(e) => setSubject(e.value.target)}
+        />
         <button>Add subject</button>
       </form>
     </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,20 @@
 import List from "../components/List";
 import Listitems from "../components/ListItems";
+import Create from "./Create";
 
 const Home = () => {
-  // const [listItem, setListItem] = setState("");
+  const [listItem, setListItem] = setState("");
 
-  // function handleChange(e) {
-  //   setListItem(e.target.value);
-  // }
+  function handleSubmit(e, inputValue) {
+    e.preventDefault();
+    newSubject = { id: listItem.length + 1, subject: inputValue };
+    setListItem([...listItem, newSubject]);
+  }
+
   return (
     <div className="homepage">
       <h2>My Study List</h2>
+      <Create handleSubmit={handleSubmit} />
       <List title="Things I need to learn">
         <Listitems />
       </List>
