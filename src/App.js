61e865fa37e299dd22399c801ef5155d96134779
Samantha Wu <samanthayywu@gmail.com>
2022-06-19 15:23:24 +0100

    home and create routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import List from "./components/List";
-import Listitems from "./components/ListItems";
 import Navbar from "./routes/Navbar";
-import "./App.css";
+import Home from "./routes/Home";
 import Create from "./routes/Create";
-import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [listItem, setListItem] = useState([{ id: 1, subject: "react" }]);
-
-  function handleSubmit(e, inputValue) {
-    e.preventDefault();
-    let newSubject = { id: listItem.length + 1, subject: inputValue };
-    setListItem([...listItem, newSubject]);
-  }
-
-  function handleDelete(id) {
-    const newList = listItem.filter((item) => item.id !== id);
-    setListItem(newList);
-  }
-
   return (
-    <div className="homepage">
-      <Navbar />
-      <h2>My Study List</h2>
-      <Create handleSubmit={handleSubmit} />
-      <List title="Things I need to learn" handleDelete={handleDelete}>
-        <Listitems />
-      </List>
-    </div>
+    <Router>
+      <div className="root">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,30 +1,30 @@
-// import List from "../components/List";
-// import Listitems from "../components/ListItems";
-// import Create from "./Create";
-// import { setState } from "react";
+import List from "../components/List";
+import Listitems from "../components/ListItems";
+import Create from "./Create";
+import { useState } from "react";
 
-// const Home = () => {
-//   const [listItem, setListItem] = setState([{ id: 1, subject: "react" }]);
+const Home = () => {
+  const [listItem, setListItem] = useState([{ id: 1, subject: "react" }]);
 
-//   function handleSubmit(e, inputValue) {
-//     e.preventDefault();
-//     let newSubject = { id: listItem.length + 1, subject: inputValue };
-//     setListItem([...listItem, newSubject]);
-//   }
+  function handleSubmit(e, inputValue) {
+    e.preventDefault();
+    let newSubject = { id: listItem.length + 1, subject: inputValue };
+    setListItem([...listItem, newSubject]);
+  }
 
-//   function handleDelete(id) {
-//     let newList = listItem.filter((item) => item.id != id);
-//     setListItem(newList);
-//   }
-//   return (
-//     <div className="homepage">
-//       <h2>My Study List</h2>
-//       <Create handleSubmit={handleSubmit} />
-//       <List title="Things I need to learn" handleDelete={handleDelete}>
-//         <Listitems />
-//       </List>
-//     </div>
-//   );
-// };
+  function handleDelete(id) {
+    const newList = listItem.filter((item) => item.id !== id);
+    setListItem(newList);
+  }
 
-// export default Home;
+  return (
+    <div className="homepage">
+      <h2>My Study List</h2>
+      <List title="Things I need to learn" handleDelete={handleDelete}>
+        <Listitems />
+      </List>
+    </div>
+  );
+};
+
+export default Home;
