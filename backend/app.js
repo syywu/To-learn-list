519e02360bd9f9af74224a3feaf6289f2b3ae8b6
Samantha Wu <samanthayywu@gmail.com>
2022-06-26 21:49:42 +0100

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,15 @@
 import express from "express";
 import cors from "cors";
 import logger from "morgan";
+import router from "./routes/user.js";
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use("/user", router);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
