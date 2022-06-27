f0de86f607c41870dbb83b85141d1fafa1c0b15e
Samantha Wu <samanthayywu@gmail.com>
2022-06-27 10:17:56 +0100

    PORT

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import logger from "morgan";
 import router from "./routes/user.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
