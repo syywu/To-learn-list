34f6bd84a306bcbfeea7a388f39acc24440df5f5
Samantha Wu <samanthayywu@gmail.com>
2022-06-27 10:11:56 +0100

    createTable

--- backend/db/scripts/createTable.js
+++ backend/db/scripts/createTable.js
@@ -1,1 +1,9 @@
-import { query } from "pg";
+import { query } from "../index.js";
+
+async function createListTable() {
+  const res = await query(`CREATE TABLE IF NOT EXISTS list(
+    user_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    subject TEXT)`);
+  console.log(res.command);
+}
+createListTable();
