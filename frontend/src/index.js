25b6596616aa56897726241cf24ace1d508f5e7d
Samantha Wu <samanthayywu@gmail.com>
2022-06-28 16:33:33 +0100

    auth index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="syywu-projects.eu.auth0.com"
+      clientId="5Ywln6FZIxcaYTBcHofEwWE5ZASc3VpY"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
